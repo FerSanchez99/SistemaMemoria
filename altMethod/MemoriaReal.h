4cfabb6ae591778bcce6db0d1c6cf7f1765af8c0
Angel-AG <miniprobecat@hotmail.com>
2020-11-30 19:08:00 -0600

    Add comments .h files

--- altMethod/MemoriaReal.h
+++ altMethod/MemoriaReal.h
@@ -12,27 +12,31 @@ class MemoriaSwap;
 
 class MemoriaReal {
     private:
-        const int tamBytes = 2048;
-        const int tamPaginas = 16;
-        int paginasLibres = tamBytes / tamPaginas;
-        bool esFIFO;
-        std::list<std::pair<int, int>> politicaRemplazoLista;
+        const int tamBytes = 2048; // Tamano total en bytes
+        const int tamPaginas = 16; // Tamano de una pagina
+        int paginasLibres = tamBytes / tamPaginas; // No. de paginas libres
+        bool esFIFO; // Si se esta utilizando una politica FIFO, sino una LRU
+        // Lista en la que decide que pagina reemplazar de acuerdo a nuestra politica, pair.first indice de memreal, pair.second idProceso
+        std::list<std::pair<int, int>> politicaRemplazoLista; 
+        // memoria, indice indica pagina, el pair.first pagina virtual del proceso, pair.second idproceso
         std::vector<std::pair<int, int>> memoria = std::vector<std::pair<int, int>>(paginasLibres, std::pair<int, int>(-1, -1));
     public:
-        MemoriaReal();
-        MemoriaReal(bool);
-        bool esPoliticaFIFO();
-        void setPoliticaFIFO(bool);
-        int getTamBytes();
-        int getPaginasLibres();
-        void setPaginasLibres(int);
-        int asignarPagina(int, int);
+        MemoriaReal(); // Constructor default
+        MemoriaReal(bool); // Constructor default con booleano que indica si es FIFO o no
+        bool esPoliticaFIFO(); // Regresa si es FIFO
+        void setPoliticaFIFO(bool); // Set politica FIFO o no
+        int getTamBytes(); // Regresa el tamano total en bytes
+        int getPaginasLibres(); // Regresa el no. de paginas libres
+        void setPaginasLibres(int); // Set el no. de paginas libres
+        int asignarPagina(int, int); // Asigna una pagina libre, recibe, pagina virtual y id de un proceso.
+        // Regresa una pagina de memoria real, (indice de pagina real, (indice de pagina virtual, idProceso))
         std::pair<int, std::pair<int, int>> intercambiaRealSwap(int, int);
+        // Asigna un proceso en memoria real
         void asignarProceso(Proceso&, MemoriaSwap&, std::unordered_map<int, Proceso>&, double&,int&);
-        void swapOut(MemoriaSwap&, Proceso&, std::pair<int, int>);
-        void liberarProceso(Proceso);
-        void limpiarMemoria();
-        void aplicarLRU(int, int);
+        void swapOut(MemoriaSwap&, Proceso&, std::pair<int, int>); // Realiza un swapout
+        void liberarProceso(Proceso); // Libera un proceso
+        void limpiarMemoria(); // Limpiar la memoria, da un reset
+        void aplicarLRU(int, int); // Aplica un LRU, mueve la pagina mas recientemente usada
 };
 
 #endif
--- altMethod/SistemaMemoria.h
+++ altMethod/SistemaMemoria.h
@@ -9,20 +9,20 @@
 
 class SistemaMemoria {
     private:
-        std::unordered_map<int, Proceso> listaProcesos; //lista que guarda los procesos
-        MemoriaReal real;//Objeto clase MemoriaReal que emula una memoria real
-        MemoriaSwap swap; //Objeto clase MemoriaSwap que emula una zona de swapping (disco)
-        Stats stats;  //Objeto clase Stats que guarda el tiempo de la 'simulación' y otras estadísticas
+        std::unordered_map<int, Proceso> listaProcesos; // mapa que guarda los procesos que se encuentran en memoria (idProceso -> Proceso)
+        MemoriaReal real;// Objeto clase MemoriaReal que emula una memoria real
+        MemoriaSwap swap; // Objeto clase MemoriaSwap que emula una zona de swapping (disco)
+        Stats stats;  // Objeto clase Stats que guarda el tiempo de la 'simulacion' y otras estadï¿½sticas
     public:
-        SistemaMemoria();    //Constructor
-        SistemaMemoria(bool);
-        Proceso getProceso(int); //regresa el proceso con cierto id
-        bool existeProceso(int); //Checa si existe o no cierto proceso
-        bool cargarProceso(int, int); //Carga el proceso a memoria principal (opción P)
-        void accederDirVirtualProceso(int, int, int); //Accede al proceso esté o no en la memoria principal (opción A)
-        void liberarProceso(int); //Opción L
-        void limpiarMemorias(); //Borra todo de las memorias
-        Stats getStat(); //regresa estadísticas del sistema de memoria
+        SistemaMemoria();    // Constructor
+        SistemaMemoria(bool); // Constructor con parametro para decidir si es FIFO o no
+        Proceso getProceso(int); // regresa el proceso con cierto id
+        bool existeProceso(int); // Checa si existe o no cierto proceso
+        bool cargarProceso(int, int); // Carga el proceso a memoria principal (opcion P)
+        void accederDirVirtualProceso(int, int, int); // Accede al proceso esto o no en la memoria principal (opcion A)
+        void liberarProceso(int); // Opcion L
+        void limpiarMemorias(); // Borra todo de las memorias
+        Stats getStat(); // regresa estadisticas del sistema de memoria
 };
 
 #endif
